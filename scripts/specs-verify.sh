10276864cdabd8fe47e0250d74e48f8cbdd0b34e
Melvin Lee <melvin.lee@iswish.nl>
2024-03-27 08:31:24 +0100

    chore(ci): 'Validate validatie functional specs' step aangepast en hernoemd
    Gebruikt scripts/specs-verify.sh script om daarin gespecificeerde features te valideren

--- scripts/specs-verify.sh
+++ scripts/specs-verify.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-npx cucumber-js -f json:./test-reports/cucumber-js/test-result-zonder-dependency-integratie.json \
-                -f summary:./test-reports/cucumber-js/test-result-zonder-dependency-integratie-summary.txt \
+npx cucumber-js -f json:./test-reports/cucumber-js/docs-features-test-result-zonder-dependency-integratie.json \
+                -f summary:./test-reports/cucumber-js/docs-features-test-result-zonder-dependency-integratie-summary.txt \
                 -f summary \
+                features/docs \
                 --tags "not @integratie"
+
+npx cucumber-js -f json:./test-reports/cucumber-js/input-validatie-personen-test-result.json \
+                -f summary:./test-reports/cucumber-js/input-validatie-personen-test-result-summary.txt \
+                -f summary \
+                features/validatie/personen \
+                --tags @fout-case
+
+npx cucumber-js -f json:./test-reports/cucumber-js/autorisatie-personen-test-result.json \
+                -f summary:./test-reports/cucumber-js/autorisatie-personen-test-result-summary.txt \
+                -f summary \
+                features/autorisatie/personen \
+                --tags @fout-case
