3d5e3bf1c45c8bace76b127a34d0fad3234389e3
Melvin Lee <melvin.lee@iswish.nl>
2024-04-03 16:13:07 +0200

    chore(referentie-api): personen endpoint mock geïmplementeerd

---
+++ src/Brp.Referentie.Api/Controllers/PersoonController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Brp.Referentie.Api.Controllers;
+
+[ApiController,
+Route("haalcentraal/api/brp")]
+public class PersoonController : ControllerBase
+{
+    [HttpPost,
+    Route("personen")]
+    public IActionResult Index([FromBody]object body)
+    {
+        return Ok(new { personen = new List<object>() });
+    }
+}
