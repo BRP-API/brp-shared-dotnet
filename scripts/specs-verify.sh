d93d6bbeb715bbe42c96a8802bdada63fde82073
MelvLee <github-action@actions-template-sync.noreply.github.com>
2024-04-03 14:22:52 +0000

    chore: sync met 'BRP-API/brp-shared' repo
    Signed-off-by: MelvLee <github-action@actions-template-sync.noreply.github.com>

--- scripts/specs-verify.sh
+++ scripts/specs-verify.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-npx cucumber-js -f json:./test-reports/cucumber-js/docs-features-test-result-zonder-dependency-integratie.json \
-                -f summary:./test-reports/cucumber-js/docs-features-test-result-zonder-dependency-integratie-summary.txt \
+npx cucumber-js -f json:./test-reports/cucumber-js/step-definitions/test-result-zonder-dependency-integratie.json \
+                -f summary:./test-reports/cucumber-js/step-definitions/test-result-zonder-dependency-integratie-summary.txt \
                 -f summary \
                 features/docs \
                 --tags "not @integratie"
-
-npx cucumber-js -f json:./test-reports/cucumber-js/input-validatie-personen-test-result.json \
-                -f summary:./test-reports/cucumber-js/input-validatie-personen-test-result-summary.txt \
-                -f summary \
-                features/validatie/personen \
-                --tags @fout-case
-
-npx cucumber-js -f json:./test-reports/cucumber-js/autorisatie-personen-test-result.json \
-                -f summary:./test-reports/cucumber-js/autorisatie-personen-test-result-summary.txt \
-                -f summary \
-                features/autorisatie/personen \
-                --tags @fout-case
