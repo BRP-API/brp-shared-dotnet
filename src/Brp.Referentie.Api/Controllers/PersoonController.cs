908da9bb29982e7ecc40b612ce1da62a3da1d374
Melvin Lee <melvin.lee@iswish.nl>
2024-04-24 10:55:39 +0200

    ci(referentie-api): toevoegen van gespecificeerde headers aan een response
    De headers staan gespecificeerd in de Data/response-headers.json bestand

--- src/Brp.Referentie.Api/Controllers/PersoonController.cs
+++ src/Brp.Referentie.Api/Controllers/PersoonController.cs
@@ -1,15 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using Newtonsoft.Json.Linq;
 namespace Brp.Referentie.Api.Controllers;
 
 [ApiController,
 Route("haalcentraal/api/brp")]
 public class PersoonController : ControllerBase
 {
+    private readonly IWebHostEnvironment _environment;
+
+    public PersoonController(IWebHostEnvironment environment)
+    {
+        _environment = environment;
+    }
+
     [HttpPost,
     Route("personen")]
-    public IActionResult Index([FromBody]object body)
+    public async Task<IActionResult> Index([FromBody]object body)
     {
+        var path = Path.Combine(_environment.ContentRootPath, "Data", "response-headers.json");
+        if(System.IO.File.Exists(path))
+        {
+            var data = await System.IO.File.ReadAllTextAsync(path);
+
+            var responseHeaders = JObject.Parse(data);
+            foreach (var header in responseHeaders)
+            {
+                HttpContext.Response.Headers.Add(header.Key, header.Value?.ToString());
+            }
+
+        }
+
         return Ok(new { personen = new List<object>() });
     }
 }
