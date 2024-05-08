2abf3b35d1eb1b5649712929492cfc34560522ea
Melvin Lee <melvin.lee@iswish.nl>
2024-05-08 16:55:14 +0200

    chore(referentie api): reisdocument controller tbv mocken downstream reisdocument response

--- src/Brp.Referentie.Api/Controllers/PersoonController.cs
+++ src/Brp.Referentie.Api/Controllers/PersoonController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json.Linq;
 namespace Brp.Referentie.Api.Controllers;
 
 [ApiController,
@@ -17,18 +16,7 @@ public class PersoonController : ControllerBase
     Route("personen")]
     public async Task<IActionResult> Index([FromBody]object body)
     {
-        var path = Path.Combine(_environment.ContentRootPath, "Data", "response-headers.json");
-        if(System.IO.File.Exists(path))
-        {
-            var data = await System.IO.File.ReadAllTextAsync(path);
-
-            var responseHeaders = JObject.Parse(data);
-            foreach (var header in responseHeaders)
-            {
-                HttpContext.Response.Headers.Add(header.Key, header.Value?.ToString());
-            }
-
-        }
+        await HttpContext.Response.AddCustomResponseHeaders(_environment);
 
         return Ok(new { personen = new List<object>() });
     }
