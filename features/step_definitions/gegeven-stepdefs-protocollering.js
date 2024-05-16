09c2a43a4b667b2e2e04aefdd5b0c6bfeb059d18
Melvin Lee <melvin.lee@iswish.nl>
2024-05-16 12:35:24 +0200

    chore(automation): gegeven-stepdefs-protocollering.js verwijderd
    obsolete door gegeven-stepdefs-downstream-api.js

--- features/step_definitions/gegeven-stepdefs-protocollering.js
+++
@@ -1,5 +0,0 @@
-const { Given } = require('@cucumber/cucumber');
-
-Given(/^de response van de downstream api heeft de volgende headers$/, function (dataTable) {
-    this.context.downstreamApiResponseHeaders = dataTable.hashes();
-});
