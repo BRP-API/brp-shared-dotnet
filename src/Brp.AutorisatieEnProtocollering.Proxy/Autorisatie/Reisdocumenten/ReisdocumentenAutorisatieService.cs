4cc0f2d50f886f03bfa548eb5aa3fb05a135b92c
Melvin Lee <melvin.lee@iswish.nl>
2024-05-27 11:52:49 +0200

    chore(autz): autorisatie op basis van gemeentecode claim

--- src/Brp.AutorisatieEnProtocollering.Proxy/Autorisatie/Reisdocumenten/ReisdocumentenAutorisatieService.cs
+++ src/Brp.AutorisatieEnProtocollering.Proxy/Autorisatie/Reisdocumenten/ReisdocumentenAutorisatieService.cs
@@ -14,34 +14,11 @@ namespace Brp.AutorisatieEnProtocollering.Proxy.Autorisatie.Reisdocumenten
             if (gemeenteCode == null)
             {
                 return NotAuthorized(title: "U bent niet geautoriseerd voor deze vraag.",
-                                     detail: "Je mag alleen reisdocumenten van inwoners uit de eigen gemeente raadplegen.",
+                                     detail: "Alleen gemeenten mogen reisdocumenten raadplegen.",
                                      code: "unauthorized",
                                      reason: "geen gemeente");
             }
 
-            return Authorized();
-        }
-
-        public override AuthorisationResult AuthorizeResponse(int afnemerCode, int? gemeenteCode, string? geleverdeGemeenteCodes)
-        {
-            if (string.IsNullOrWhiteSpace(geleverdeGemeenteCodes))
-            {
-                return NotAuthorized(title: "U bent niet geautoriseerd voor deze vraag.",
-                                     detail: "Je mag alleen reisdocumenten van inwoners uit de eigen gemeente raadplegen.",
-                                     code: "unauthorized",
-                                     reason: $"er zijn geen gemeentecodes van houders geleverd");
-            }
-
-            foreach (var gemCode in geleverdeGemeenteCodes.Split(','))
-            {
-                if(int.Parse(gemCode) != gemeenteCode)
-                {
-                    return NotAuthorized(title: "U bent niet geautoriseerd voor deze vraag.",
-                                         detail: "Je mag alleen reisdocumenten van inwoners uit de eigen gemeente raadplegen.",
-                                         code: "unauthorized",
-                                         reason: $"één of meerdere houders zijn ingeschreven in gemeente {gemCode}");
-                }
-            }
             return Authorized();
         }
     }
