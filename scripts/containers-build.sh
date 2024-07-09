c7495c33ae29295862d26c076568c27b4277038b
Melvin Lee <melvin.lee@iswish.nl>
2024-07-09 18:45:52 +0200

    ci: ci-only optie toegevoegd en typo gefixt in containers-build.sh

--- scripts/containers-build.sh
+++ scripts/containers-build.sh
@@ -3,7 +3,7 @@
 MODE=$1
 
 if [ "$MODE" = "ci" ]; then
-    docker buildx create --name container --driver=docker-container
+    docker buildx create --name container --driver docker-container --use
     docker compose -f .docker/docker-compose-ci.yml build
 else
     docker compose -f src/docker-compose.yml build
