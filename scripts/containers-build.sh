5e056ce73b46d5ddce098643deacbf77de42cc7e
Melvin Lee <melvin.lee@iswish.nl>
2024-07-10 15:11:59 +0200

    chore: default-load driver optie toegevoegd om container images toe te voegen in registry

--- scripts/containers-build.sh
+++ scripts/containers-build.sh
@@ -3,9 +3,9 @@
 MODE=$1
 
 if [ "$MODE" = "ci" ]; then
-    docker buildx create --name container --driver docker-container --use
+    docker buildx create --name container --driver docker-container --use --driver-opt default-load=true
     docker compose -f .docker/docker-compose-ci.yml build
-    docker buildx rm --force container
+    # docker buildx rm --force container
 else
     docker compose -f src/docker-compose.yml build
 fi
