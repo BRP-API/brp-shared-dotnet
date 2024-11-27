1d3e89c254d8ad836a87b84c0bbc57e9ae775db1
Melvin Lee <melvin.lee@iswish.nl>
2024-11-27 16:38:33 +0100

    chore: datumMapper & GeboorteBasisProfile unit tests toegevoegd

---
+++ src/Brp.Shared.DtoMappers.Tests/Profiles/GeboorteBasisProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Brp.Shared.DtoMappers.Mappers;
+using FluentAssertions;
+
+namespace Brp.Shared.DtoMappers.Tests.Profiles;
+
+public class GeboorteBasisProfile
+{
+    private static IMapper CreateSut() => AutomapperUnderTestFactory.CreateSut<DtoMappers.Profiles.GeboorteBasisProfile>();
+
+    [Fact]
+    public void ShouldMapWithoutThrowing()
+    {
+        BrpDtos.GeboorteBasis input = new ()
+        {
+            Datum = DateTime.Today.ToString("yyyyMMdd"),
+        };
+
+        BrpApiDtos.GeboorteBasis expected = new ()
+        {
+            Datum = DateTime.Today.ToString("yyyyMMdd").Map()
+        };
+
+        CreateSut().Map<BrpApiDtos.GeboorteBasis>(input).Should().BeEquivalentTo(expected);
+    }
+}
--- src/Brp.Shared.DtoMappers/Mappers/DatumMapper.cs
+++ src/Brp.Shared.DtoMappers/Mappers/DatumMapper.cs
@@ -7,6 +7,21 @@ namespace Brp.Shared.DtoMappers.Mappers;
 public static class DatumMapper
 {
     private static readonly Regex GbaDatumRegex = new("^(?<jaar>[0-9]{4})(?<maand>[0-9]{2})(?<dag>[0-9]{2})$", RegexOptions.None, TimeSpan.FromMilliseconds(100));
+    private static readonly Dictionary<int, string> MaandDictionary = new ()
+    {
+        {1, "januari" },
+        {2, "februari" },
+        {3, "maart" },
+        {4, "april" },
+        {5, "mei" },
+        {6, "juni" },
+        {7, "juli" },
+        {8, "augustus" },
+        {9, "september" },
+        {10, "oktober" },
+        {11, "november" },
+        {12, "december" }
+    };
 
     public static AbstractDatum Map(this string datum)
     {
@@ -20,52 +35,37 @@ public static class DatumMapper
             AbstractDatum retval = new DatumOnbekend();
             if (jaar != 0 && maand != 0 && dag != 0)
             {
-                retval = new VolledigeDatum() { Datum = new DateTime(jaar, maand, dag) };
+                retval = new VolledigeDatum()
+                {
+                    Datum = new DateTime(jaar, maand, dag, 0, 0, 0, DateTimeKind.Local),
+                    LangFormaat = $"{dag} {MaandDictionary[maand]} {jaar}"
+                };
             }
             if (jaar != 0 && maand != 0 && dag == 0)
             {
-                retval = new JaarMaandDatum() { Jaar = jaar, Maand = maand };
+                retval = new JaarMaandDatum()
+                {
+                    Jaar = jaar,
+                    Maand = maand,
+                    LangFormaat = $"{MaandDictionary[maand]} {jaar}"
+                };
             }
             if (jaar != 0 && maand == 0 && dag == 0)
             {
-                retval = new JaarDatum { Jaar = jaar };
+                retval = new JaarDatum
+                {
+                    Jaar = jaar,
+                    LangFormaat = $"{jaar}"
+                };
             }
 
-            retval.LangFormaat = retval.LangFormaat();
             return retval;
 
         }
+
         return new DatumOnbekend
         {
             LangFormaat = "onbekend"
-        };
-    }
-
-    public static string? LangFormaat(this AbstractDatum datum)
-    {
-        var maand = new Dictionary<int, string>
-        {
-            {1, "januari" },
-            {2, "februari" },
-            {3, "maart" },
-            {4, "april" },
-            {5, "mei" },
-            {6, "juni" },
-            {7, "juli" },
-            {8, "augustus" },
-            {9, "september" },
-            {10, "oktober" },
-            {11, "november" },
-            {12, "december" }
-        };
-
-        return datum switch
-        {
-            VolledigeDatum d => $"{d.Datum!.Value.Day} {maand[d.Datum!.Value.Month]} {d.Datum!.Value.Year}",
-            JaarMaandDatum d => $"{maand[d.Maand!.Value]} {d.Jaar}",
-            JaarDatum d => $"{d.Jaar}",
-            DatumOnbekend => "onbekend",
-            _ => null
         };
     }
 }
