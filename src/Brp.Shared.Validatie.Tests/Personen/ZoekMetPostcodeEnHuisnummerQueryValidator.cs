c0e2ed9b0fbfe2cb356b81f7151d620b35e4d496
Niels Korporaal <33909530+nielskorporaal@users.noreply.github.com>
2025-03-12 14:36:25 +0100

    Zoek personen met postcode huisnummer en geslachtsnaam (#94)
    * test: protocollering op geslachtsnaam bij zoeken met postcode huisnummer en geslachtsnaam
    * feat: validatie op optioneel veld geslachtsnaam bij zoeken met pc en hn
    * test: fout cases bij zoeken met pc en hn en veld geslachtsnaam
    * test: protocollering geboortedatum bij zoeken met pc+hn en geboortedatum
    * refactor: geslachtsnaam optioneel en verplicht validators refactored naar 1 geslachtsnaam validator
    * chore: bump minor version nav nieuwe functionaliteit
    * fix: include geboortedatum datumvalidator
    * fix(input validatie): ongeldige datums wordt correct gevalideerd

---
+++ src/Brp.Shared.Validatie.Tests/Personen/ZoekMetPostcodeEnHuisnummerQueryValidator.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Newtonsoft.Json.Linq;
+
+namespace Brp.Shared.Validatie.Tests.Personen;
+
+public class ZoekMetPostcodeEnHuisnummerQueryValidator
+{
+    private static Validatie.Personen.ZoekMetPostcodeEnHuisnummerQueryValidator CreateSut() => new();
+
+    [Fact]
+    public void ShouldFailWhenRequiredParametersAreMissing()
+    {
+        var input = JObject.Parse("{}");
+
+        var result = CreateSut().Validate(input);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().HaveCount(3);
+        result.Errors[0].ErrorMessage.Should().Be("required||Parameter is verplicht.");
+        result.Errors[0].PropertyName.Should().Be("postcode");
+        result.Errors[1].ErrorMessage.Should().Be("required||Parameter is verplicht.");
+        result.Errors[1].PropertyName.Should().Be("huisnummer");
+        result.Errors[2].ErrorMessage.Should().Be("required||Parameter is verplicht.");
+        result.Errors[2].PropertyName.Should().Be("fields");
+    }
+
+    [Fact]
+    public void ShouldFailWhenOneOrMorePropertiesAreNotSpecifiedAsParameters()
+    {
+        var input = JObject.Parse("{\"type\": \"ZoekMetPostcodeEnHuisnummer\", \"postcode\": \"1234AA\", \"huisnummer\": \"1\", \"voornamen\": \"\", \"fields\": [ \"burgerservicenummer\" ]}");
+
+        var result = CreateSut().Validate(input);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle();
+        result.Errors[0].ErrorMessage.Should().Be("voornamen||unknownParam||Parameter is niet verwacht.");
+    }
+}
--- src/Brp.Shared.Validatie/Validators/GeslachtsnaamValidator.cs
+++ src/Brp.Shared.Validatie/Validators/GeslachtsnaamValidator.cs
@@ -3,7 +3,7 @@ using Newtonsoft.Json.Linq;
 
 namespace Brp.Shared.Validatie.Validators;
 
-public class GeslachtsnaamVerplichtValidator : AbstractValidator<JObject>
+public class GeslachtsnaamValidator : AbstractValidator<JObject>
 {
     private const string ParameterNaam = "geslachtsnaam";
 
@@ -11,12 +11,17 @@ public class GeslachtsnaamVerplichtValidator : AbstractValidator<JObject>
     const string GeslachtsnaamPattern = @"^[a-zA-Z0-9À-ž \.\-\']{1,200}$|^[a-zA-Z0-9À-ž \.\-\']{3,199}\*{1}$";
     const string GeslachtsnaamPatternErrorMessage = $"pattern||Waarde voldoet niet aan patroon {GeslachtsnaamPattern}.";
 
-    public GeslachtsnaamVerplichtValidator()
+    public GeslachtsnaamValidator(bool isVerplichtVeld = false)
     {
-        RuleFor(x => x.Value<string>(ParameterNaam))
-            .Cascade(CascadeMode.Stop)
-            .NotEmpty().WithMessage(RequiredErrorMessage)
-            .Matches(GeslachtsnaamPattern).WithMessage(GeslachtsnaamPatternErrorMessage)
+        var rule = RuleFor(x => x.Value<string>(ParameterNaam))
+                    .Cascade(CascadeMode.Stop);
+
+        if (isVerplichtVeld)
+        {
+            rule.NotEmpty().WithMessage(RequiredErrorMessage);
+        }
+            
+        rule.Matches(GeslachtsnaamPattern).WithMessage(GeslachtsnaamPatternErrorMessage)
             .OverridePropertyName(ParameterNaam);
     }
 }
