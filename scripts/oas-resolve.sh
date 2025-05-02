27098515dcc019ed3c51ab5b6e1184bbb0c4d7c6
MelvLee <github-action@actions-template-sync.noreply.github.com>
2025-05-02 00:16:28 +0000

    chore: sync met 'BRP-API/brp-shared' repo
    Signed-off-by: MelvLee <github-action@actions-template-sync.noreply.github.com>

--- scripts/oas-resolve.sh
+++ scripts/oas-resolve.sh
@@ -2,4 +2,5 @@
 
 npx redocly bundle ./specificatie/referentie-data-api/openapi.yaml -o ./specificatie/referentie-data-api/resolved/openapi.yaml
 npx redocly bundle ./specificatie/referentie-gezag-api/openapi.yaml -o ./specificatie/referentie-gezag-api/resolved/openapi.yaml
+npx redocly bundle ./specificatie/referentie-gezag-api/openapi-v2.yaml -o ./specificatie/referentie-gezag-api/resolved/openapi-v2.yaml
 npx redocly bundle ./specificatie/referentie-informatie-api/openapi.yaml -o ./specificatie/referentie-informatie-api/resolved/openapi.yaml
