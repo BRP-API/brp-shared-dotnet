0dc1b6d9812fc2c11f5b76f868ea8ea125d09af9
Melvin Lee <melvin.lee@iswish.nl>
2025-10-22 13:34:21 +0200

    feat(a&p): kmar geautoriseerd voor reisdocumenten

--- src/Brp.AutorisatieEnProtocollering.Proxy/Autorisatie/Reisdocumenten/AutorisatieService.cs
+++ src/Brp.AutorisatieEnProtocollering.Proxy/Autorisatie/Reisdocumenten/AutorisatieService.cs
@@ -11,15 +11,32 @@ namespace Brp.AutorisatieEnProtocollering.Proxy.Autorisatie.Reisdocumenten
 
         public override AuthorisationResult Authorize(int afnemerCode, int? gemeenteCode, string requestBody)
         {
-            if (!AfnemerIsGemeente(afnemerCode, gemeenteCode))
+            if (AfnemerIsGemeente(afnemerCode, gemeenteCode) || AfnemerIsKmar(afnemerCode))
             {
-                return NotAuthorized(title: "U bent niet geautoriseerd voor deze vraag.",
-                                     detail: "Alleen gemeenten mogen reisdocumenten raadplegen.",
-                                     code: "unauthorized",
-                                     reason: "geen gemeente");
+                return Authorized();
             }
 
-            return Authorized();
+            return NotAuthorized(title: "U bent niet geautoriseerd voor deze vraag.",
+                                    detail: "Alleen gemeenten mogen reisdocumenten raadplegen.",
+                                    code: "unauthorized",
+                                    reason: "geen gemeente");
+        }
+
+        private bool AfnemerIsKmar(int afnemerCode)
+        {
+            const int kmarAfnemerCode = 720402;
+
+            if (afnemerCode == kmarAfnemerCode)
+            {
+                if (AutorisatieLog != null)
+                {
+                    AutorisatieLog.Gemeente = $"afnemer: {afnemerCode} is KMAR";
+                }
+
+                return true;
+            }
+
+            return false;
         }
     }
 }
