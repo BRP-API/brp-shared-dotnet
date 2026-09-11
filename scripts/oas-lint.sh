b2c78a809b07f81106bf4091b47ffe3c8279e0c6
MelvLee <github-action@actions-template-sync.noreply.github.com>
2026-09-11 00:18:53 +0000

    chore: sync met 'BRP-API/brp-shared' repo
    Signed-off-by: MelvLee <github-action@actions-template-sync.noreply.github.com>

--- scripts/oas-lint.sh
+++ scripts/oas-lint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-npx redocly lint ./specificatie/referentie-data-api/openapi.yaml
-npx redocly lint ./specificatie/referentie-informatie-api/openapi.yaml
-npx redocly lint ./specificatie/referentie-gezag-api/openapi.yaml
-npx redocly lint ./specificatie/referentie-gezag-api/openapi-v2.yaml
+npx --ignore-scripts redocly lint ./specificatie/referentie-data-api/openapi.yaml
+npx --ignore-scripts redocly lint ./specificatie/referentie-informatie-api/openapi.yaml
+npx --ignore-scripts redocly lint ./specificatie/referentie-gezag-api/openapi.yaml
+npx --ignore-scripts redocly lint ./specificatie/referentie-gezag-api/openapi-v2.yaml
